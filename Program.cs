9214659f7b6893a2249ff4859898b7feaba66ca5
Evgen <zhukowskyeu@yandex.ru>
2022-09-19 22:35:45 +0500

    готовая программа

--- Program.cs
+++ Program.cs
@@ -3,30 +3,33 @@
 // Программа, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
 
 
-string[] array(string[]newarray)
+string[] createArray(string[]newarray)
 {
     
-       string[] RandomArray = new newarray.Length;
+       string[] RandomArray = new string[newarray.Length];
        int N = newarray.Length;
        int c = 0;
     for (int i = 0; i < N; i++)
     {
-        if (string < 4)
-    RandomArray[c] == newarray[i];
-           
+        if (newarray[i].Length < 4)
+             {
+   RandomArray[c] = newarray[i];
+             }    
     }
     return RandomArray;
 
 }
-
-
 
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++ )
     {
       
-        Console.WriteLine(string[i]);
+        Console.WriteLine(array[i]);
     }
     
 }
+
+string [] arr  = {"1234", "1567", "-2", "computer science"};
+
+PrintArray(createArray(arr));
